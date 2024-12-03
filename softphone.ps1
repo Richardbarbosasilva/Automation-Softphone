5ee218977e81d110e6aa45b3d7bb5ea3948c5749
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2024-12-03 17:20:01 -0400

    adjusting paths and comments

--- softphone.ps1
+++ softphone.ps1
@@ -1,10 +1,10 @@
 # first get current username and hostname to later save the file using it
 
 
-unblock-file -Path "\\arquivosdti.clickip.local\automacao_dados\pyinstall\softphone.ps1" -WarningAction Ignore
+unblock-file -Path "UNC-path-to-this-script" -WarningAction Ignore
 
 
-##################################### STEP 1 = GET HOSTNAME #########################################
+###################################################################################### STEP 1 = GET HOSTNAME #########################################################################################################
 
 
 $hostname = [System.Environment]::MachineName
@@ -16,12 +16,12 @@ $currentuser = $env:USERNAME
     Write-Host "$currentuser"
 
 
-########## STEP 2 = create local folder and Copy files from arquivosdti to local disk ########################### 
+######################################################################## STEP 2 = create local folder and Copy files from arquivosdti to local disk #################################################################### 
 
 
 # path to softphone network folder
 
-$networkfoldersoftphone = "\\arquivosdti.clickip.local\automacao_dados\softphone source\ClickIP Softphone"
+$networkfoldersoftphone = "UNC-path-to-softphonefolder"
 
 # Path to softphone localhost folder
 
@@ -29,7 +29,7 @@ $destinysoftphone = "C:\"
 
 # Path to softphone exported files
 
-$destinysoftphonefiles = "C:\ClickIP Softphone"
+$destinysoftphonefiles = "path-to-your-local-folder-softphone"
 
 # Verify if the network's softphone custom folder does exist
 
@@ -39,13 +39,13 @@ if (Test-Path $networkfoldersoftphone -PathType Container) {
 
 # create local folder to ClickIP Softphone
    
-   New-Item -Path "C:\ClickIP Softphone" -ItemType Directory
+   New-Item -Path "path-to-softphone-folder" -ItemType Directory
 
-   Write-Host "Created ClickIP Softphone folder on C:\"
+   Write-Host "Created Softphone folder on C:\"
 
 } else {
 
-   Write-Host "Could not create ClickIP Softphone folder on C:\, verify permissions, path or maybe it already does exists."
+   Write-Host "Could not create Softphone folder on C:\, verify permissions, path or maybe it already does exists."
 
 }
 
@@ -85,9 +85,9 @@ if ($files.Count -gt 0) {
 Write-Host "Copied files from UNC path to local Folder"
 
 
-########################### STEP 3: Create symbolic link on Desktop ####################################################
+########################################################################### STEP 3: Create symbolic link on Desktop #######################################################################################################
 
-$prefileini = "C:\ClickIP Softphone\ClickIP Softphone.ini" 
+$prefileini = "local-path-to-your-ini" 
 
 if (Test-Path $prefileini -PathType leaf) {
 
@@ -95,7 +95,7 @@ if (Test-Path $prefileini -PathType leaf) {
 
 # delete pre setted ini file from UNC to local and rename it
   
-  Remove-Item -Path "C:\ClickIP Softphone\ClickIP Softphone.ini"           
+  Remove-Item -Path "path-to-remove-old-link (if exist)"           
 
 } else {
 
@@ -103,13 +103,13 @@ if (Test-Path $prefileini -PathType leaf) {
 
 }
 
-###########################################################################
+##########################################################################################################################################################################################################################
 
-$ClickipsoftphoneExecutable = "C:\ClickIP Softphone\ClickIP Softphone.exe"
+$ClickipsoftphoneExecutable = "local-path-to-softphone-executable"
 
 Write-Host "Creating symbolic link (shortcut) on user's desktop from local installation folder"
  
-$shortcutPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("Desktop"), "ClickIP Softphone.lnk")
+$shortcutPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("Desktop"), "(yoursoftphone) Softphone.lnk")
 
 $shell = New-Object -ComObject WScript.Shell
 
@@ -122,18 +122,18 @@ $shortcut.Save()
 Write-Host "Shortcut created on $shortcutPath"
 
 
-########################### STEP 4: Delete old Clickip softphone folders ####################################################
+############################################################################# STEP 4: Delete old Clickip softphone folders ################################################################################################
 
-
+# only if exist
 # Define the variables
 
-#$Localappdataclickip = "C:\Users\$currentuser\AppData\Local\ClickIP Softphone"
+#$Localappdataclickip = "appdata-softphone-path-folder"
 
-#$Roaminginitfolder = "C:\Users\$currentuser\AppData\Roaming\ClickIP"
+#$Roaminginitfolder = "roaming-softphone-path-folder"
 
 
 
- #Delete ClickIP folder inside roaming appdata
+ # Delete ClickIP folder inside roaming appdata
 
 #if (Test-Path $Roaminginitfolder -PathType Container) {
 
@@ -175,25 +175,25 @@ $currentuser = $env:USERNAME
 
     Write-Host "$currentuser"
 
-$Roaminginitdestiny = "C:\ClickIP Softphone" 
+$Roaminginitdestiny = "path-to-your-local-softphone-folder" 
 
 if (Test-Path $Roaminginitdestiny -PathType Container) {
 
    Write-Host "$Roaminginitfolder with user's softphone configuration settings found!"
 
-   Remove-Item "C:\ClickIP Softphone\ClickIP Softphone.ini"
+   Remove-Item "path-to-old-softphone-symbolic-link"
    
-   Write-Host "ClickIP Softphone.ini deleted (pre stored)"
+   Write-Host "Softphone.ini deleted (pre stored)"
 
 # Copy user ini file from UNC to local and rename it
                   
-   Copy-Item -Path "\\arquivosdti.clickip.local\automacao_dados\softphone configfiles\ClickIP-$currentuser.ini" -Destination "C:\ClickIP Softphone" -Force
+   Copy-Item -Path "path-to-your-softphone-ini-file-$currentuser.ini" -Destination "path-to-softphone-local-folder" -Force
 
    Write-Host "File imported"
 
-   Rename-Item -Path "C:\ClickIP Softphone\ClickIP-$currentuser.ini" -NewName "C:\ClickIP Softphone\ClickIP Softphone.ini" 
+   Rename-Item -Path "path-to-your-softphone-ini-file-$currentuser.ini" -NewName "path-to-your-softphone-ini-file-$currentuser.ini\(newfilename).ini" 
 
-   Write-Host "init file was renamed from UNC path to new softphone version name (ClickIP Softphone)!"
+   Write-Host "init file was renamed from UNC path to new softphone version name (Softphone)!"
 
 } else {
 
